ce49592d21a30e885236f924711065bed033550d
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-09-10 16:53:07 +0545

    Init repo with hot module reloading

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<App />, rootEl);
+
+if(module.hot) {
+    module.hot.accept('./App', () => {
+        ReactDOM.render(<App />, rootEl);  
+    });
+}
+registerServiceWorker();
