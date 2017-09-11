81e4350c91a737305f974e79f0b59ccf0c23871a
AJRedDevil <ajan.shresh@gmail.com>
2017-09-11 10:31:06 +0545

    UI for the voting screen and its test-cases

--- src/components/Voting.test.jsx
+++ src/components/Voting.test.jsx
@@ -35,4 +35,39 @@ describe('Voting', () => {
 
         expect(votedWith).to.equal('Titanic');
     });
+
+    test('disables buttons when user has voted', () => {
+        const component = renderIntoDocument(
+            <Voting pair={['Titanic', 'Shutter Island']}
+                    hasVoted='Titanic'/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons.length).to.equal(2);
+        expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+        expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+    });
+
+    test('adds label to the voted entry', () => {
+        const component = renderIntoDocument(
+            <Voting pair={['Titanic', 'Shutter Island']}
+                    hasVoted='Titanic'/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons[0].textContent).to.contain('Voted');
+    });
+
+    test('renders just the winner when there is one', () => {
+        const component = renderIntoDocument(
+            <Voting winner='Titanic' />
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+        expect(buttons.length).to.equal(0);
+
+        const winner = ReactDOM.findDOMNode(component.refs.winner);
+        expect(winner).to.be.ok;
+        expect(winner.textContent).to.contain('Titanic');
+    });
+
 });
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
+import './styles.css';
+
 const pair = ['Titanic', 'Shutter Island'];
 
 const rootEl = document.getElementById('root');
 
-ReactDOM.render(<Voting pair={pair} />, rootEl);
+ReactDOM.render(
+    <Voting
+        pair={pair}
+        winner='Titanic'
+    />,
+    rootEl);
 
 if(module.hot) {
     module.hot.accept('./components/Voting', () => {
